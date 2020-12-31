f0824df7ba7e4a5ff68be5c115b085e5e836ff4f
Owl-ofMinerva <0.miguel.dominguez@gmail.com>
2020-12-31 15:20:51 +0100

    Header, Footer, Navbar, Hero, animated typography, Bootswatch Boootstrap

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../../css/style.css';
+
+const hero = () => {
+	return (
+		<div className="header">
+			<div className="header__text-box">
+				<h1 className="heading-primary">
+					<span className="heading-primary--main">Owl of Minerva</span>
+					<span className="heading-primary--sub">Art Studio</span>
+				</h1>
+				<div className="btn-container">
+					<a href="#" className="btn btn--white btn--animated">
+						Lorem
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default hero;
